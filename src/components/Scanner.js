108c3bc890c824dbc75ee9617bc5f2317c69ffcf
Matt Thurling <matt@cpdfoteachers.com>
2020-01-06 17:42:46 +0000

    Revert scanner to class component

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Grid, Card, Typography } from '@material-ui/core'
 // import Switch from '@material-ui/core/Switch';
 
-export default props => {
+class Scanner extends Component {
 
-  // const [cameraId, setCameraId] = useState('')
-
-  // const showScanner = () => {
-    const Dynamsoft = window.Dynamsoft;
-    let scanner = null;
+  componentDidMount() {
+    const Dynamsoft = window.Dynamsoft
+    let scanner = null
     Dynamsoft.BarcodeScanner.createInstance({
         UIElement: document.getElementById('video-container'),
-        onUnduplicatedRead: (txt, result) => {handleScan(txt);}
+        onUnduplicatedRead: (txt, result) => {this.handleScan(txt)}
     }).then(s => {
-        scanner = s;
+        scanner = s
         // getCamera()
         scanner.show()//.then(() => scanner.play(cameraId))
         .catch(ex=>{
-            console.log(ex);
-            alert(ex.message || ex);
-            scanner.hide();
+            console.log(ex)
+            alert(ex.message || ex)
+            scanner.hide()
         });
     });
-  // };
+  }
 
-  const handleScan = (t) => {
+  componentWillUnmount() {
+    console.log("Time to unmount")
+  }
+
+  handleScan(t) {
     window.location.href = '/product?gtin=' + t
   }
 
@@ -48,28 +50,23 @@ export default props => {
   //   .then(i => setCameraId(i))
   // }
 
+  render() {
+    return (
 
-  return (
+      
+            
+            <video
+              id='video-container'
+              className='dbrScanner-video'
+              style={{ width: '90%', paddingTop: 20 }}>
+            </video>
 
-    <Grid container spacing={5} style={{ padding: 20 }} alignItems="stretch">
+            
 
-      <Grid item xs={12} lg={4}>
-        <Card align="center" style={{ height: '100%' }}>
-          <Typography variant="overline">
-              Scanner
-          </Typography>
-          
-          <video
-            id='video-container'
-            className='dbrScanner-video'
-            style={{ width: '90%', paddingTop: 20 }}></video>
-        </Card>
-          
-      </Grid>
-        
-    </Grid> 
-     
-  );
-
+       
+    )
+  }
 }
+
+export default Scanner
 
